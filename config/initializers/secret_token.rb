d6b21c4938a13a7cc5f7d0c6287568d031edf9cd
Ken D'Ambrosio <ken@jots.org>
2013-11-12 04:41:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+R4rmusic1::Application.config.secret_key_base = '247f26e6767d11420857234939770eeab1ced3e88cc1178e520bc79bd9fc1faaac162aa04076b541abe5dd717b17bd2623cac403e8a8c96ef693e68dea131c72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+R4rmusic1::Application.config.session_store :cookie_store, key: '_r4rmusic1_session'
